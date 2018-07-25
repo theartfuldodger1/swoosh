64a24242c01bcefa804e56bd593a796f9763f217
Cordell Hurst <16294570+theartfuldodger1@users.noreply.github.com>
2018-07-24 21:20:45 -0400

    started finishActivity

---
+++ app/src/main/java/com/littlecitycreations/swoosh/Controller/FinishActivity.kt
@@ -0,0 +1,12 @@
+package com.littlecitycreations.swoosh.Controller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FinishActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_finish)
+    }
+}
