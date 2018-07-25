263db14e1a6d898ef99db6a567ed1c4fb7a1c638
Cordell Hurst <16294570+theartfuldodger1@users.noreply.github.com>
2018-07-24 23:23:18 -0400

    started Player class

--- app/src/main/java/com/littlecitycreations/swoosh/Controller/LeagueActivity.kt
+++ app/src/main/java/com/littlecitycreations/swoosh/Controller/LeagueActivity.kt
@@ -4,13 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import com.littlecitycreations.swoosh.Utilities.EXTRA_LEAGUE
+import com.littlecitycreations.swoosh.Model.Player
 import com.littlecitycreations.swoosh.R
+import com.littlecitycreations.swoosh.Utilities.EXTRA_PLAYER
 import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
 
-    var selectedLeague = ""
+    var player = Player("", "")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,27 +22,27 @@ class LeagueActivity : BaseActivity() {
         womensLeagueBtn.isChecked = false
         coedLeagueBtn.isChecked = false
 
-        selectedLeague = "mens"
+        player.league = "mens"
     }
 
     fun onWomensClicked(view: View){
         mensLeagueBtn.isChecked = false
         coedLeagueBtn.isChecked = false
 
-        selectedLeague = "womens"
+        player.league = "womens"
     }
 
     fun onCoedClicked(view: View){
         mensLeagueBtn.isChecked = false
         womensLeagueBtn.isChecked = false
 
-        selectedLeague = "co-ed"
+        player.league = "co-ed"
     }
 
     fun leagueNextClicked(view: View){
-        if(selectedLeague != ""){
+        if(player.league != ""){
             val skillActivity = Intent(this, SkillActivity::class.java)
-            skillActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
+            skillActivity.putExtra(EXTRA_PLAYER, player)
             startActivity(skillActivity)
         }
         else{
--- app/src/main/java/com/littlecitycreations/swoosh/Controller/SkillActivity.kt
+++ app/src/main/java/com/littlecitycreations/swoosh/Controller/SkillActivity.kt
@@ -4,41 +4,39 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import com.littlecitycreations.swoosh.Utilities.EXTRA_LEAGUE
-import com.littlecitycreations.swoosh.Utilities.EXTRA_SKILL
+import com.littlecitycreations.swoosh.Model.Player
+import com.littlecitycreations.swoosh.Utilities.EXTRA_PLAYER
 import com.littlecitycreations.swoosh.R
 import kotlinx.android.synthetic.main.activity_skill.*
 
 class SkillActivity : BaseActivity() {
 
-    var league = ""
-    var skill = ""
+    lateinit var player : Player
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
-        league = intent.getStringExtra(EXTRA_LEAGUE)
-        println("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO $league")
+        player = intent.getStringExtra(EXTRA_PLAYER)
+        println("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO $player.league")
     }
 
     fun onBallerClick(view: View) {
         beginnerSkillBtn.isChecked = false
-        skill = "baller"
+        player.skill = "baller"
     }
 
     fun onBeginnerClick(view: View) {
         ballerSkillBtn.isChecked = false
-        skill = "beginner"
+        player.skill = "beginner"
     }
 
     fun onSkillFinishClicked(view: View) {
         val finishActivity = Intent(this, FinishActivity::class.java)
         startActivity(finishActivity)
 
-        if (skill != ""){
+        if (player.skill != ""){
             val finishingActivity = Intent(this, FinishActivity::class.java)
-            finishingActivity.putExtra(EXTRA_LEAGUE, league)
-            finishingActivity.putExtra(EXTRA_SKILL, skill)
+            finishingActivity.putExtra(EXTRA_PLAYER, player)
             startActivity(finishingActivity)
         } else {
             Toast.makeText(this, "Please select a skill level", Toast.LENGTH_SHORT).show()
